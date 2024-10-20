e2871341e274848c960f70058cf367cef5b1ac0e
Bunthet SAY <bunthet.say@presta.tessi.fr>
2024-10-20 10:58:43 +0200

    init project

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ClamAVWindowsUtils
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
